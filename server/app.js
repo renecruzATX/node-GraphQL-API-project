fe9e249e56a3ccde62cfde8eb769d9261acc03ad
Rene <renecruzatx@outlook.com>
2019-09-04 15:14:41 -0500

    Added a login query and a resolver

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use(
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use(cors());
+app.use(cors() );
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
 //     res.setHeader(
