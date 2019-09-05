c57c85da0a027ecfc636deae7c3035d6f9c89033
Rene <renecruzatx@outlook.com>
2019-09-05 15:26:00 -0500

    added a create post mutation

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports = {
     createUser: async function({ userInput }, req) {
@@ -59,5 +60,39 @@ module.exports = {
             {expiresIn: '1h'}
         );
         return {token: token, userId: user._id.toString()};
+    },
+    createPost: async function({postInput}, req) {
+        const errors = [];
+        if (
+            validator.isEmpty(postInput.title) || 
+            !validator.isLength(postInput.title, {min: 5})
+        ) {
+            errors.push({message: 'Please add a title with more than 5 characters.'})
+        }
+        if (
+            validator.isEmpty(postInput.content) || 
+            !validator.isLength(postInput.content, {min: 5})
+        ) {
+            errors.push({message: 'Please create a post with more than 5 characters.'})
+        }
+        if (errors.length > 0) {
+            const error = new Error('Invalid Input');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+        const post = new Post({
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl
+        });
+        const createdPost = await post.save();
+        // Add post to user posts later
+        return {
+            ...createdPost._doc,
+            _id: createdPost._id.toString(),
+            createdAt: createdPost.createdAt.toISOString(),
+            updatedAt: createdPost.updatedAt.toISOString()
+        };
     }
 };
