b7da1c689635dcb4b910de806925d658309fe343
Rene <renecruzatx@outlook.com>
2019-09-12 16:24:17 -0500

    Added functionality for updating posts with graphql

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -158,5 +158,54 @@ module.exports = {
             createdAt: post.createdAt.toISOString(),
             updatedAt: post.updatedAt.toISOString()
         }
-    }
+    },
+    updatePost: async function({id, postInput}, req) {
+        if(!req.isAuth) {
+            const error = new Error('Not Authenticated!');
+            error.code = 401;
+            throw error;
+        }
+        const post = await Post.findById(id).populate('creator');
+        if (!post) {
+            const error = new Error ('No post found!');
+            error.code = 404;
+            throw error;
+        }
+        if (post.creator._id.toString() !== req.userId.toString()) {
+            const error = new Error('Not Authenticated!');
+            error.code = 403;
+            throw error;
+        }
+        const errors = [];
+        if (
+            validator.isEmpty(postInput.title) || 
+            !validator.isLength(postInput.title, {min: 5})
+        ) {
+            errors.push({message: 'Please add a title with more than 5 characters.'})
+        }
+        if (
+            validator.isEmpty(postInput.content) || 
+            !validator.isLength(postInput.content, {min: 5})
+        ) {
+            errors.push({message: 'Please create a post with more than 5 characters.'})
+        }
+        if (errors.length > 0) {
+            const error = new Error('Invalid Input');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        } 
+        post.title = postInput.title;
+        post.content = postInput.content;
+        if (postInput.imageUrl !== 'undefined') {
+            post.imageUrl = postInput.imageUrl;
+        }
+        const updatedPost = await post.save();
+        return {
+            ...updatedPost._doc,
+            _id: updatedPost._id.toString(),
+            createdAt: updatedPost.createdAt.toISOString(),
+            updatedAt: updatedPost.updatedAt.toISOString()
+        };
+    }   
 };
